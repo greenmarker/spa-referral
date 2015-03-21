f1ad951d823f816536cd2be692964bb21f9001a3
animos3d <kasia@kasia-VAIO>
2015-03-21 12:37:54 +0100

    Class implemated interface

---
+++ src/main/java/pl/hackerspace/nfz/SpaReferralReader.java
@@ -0,0 +1,10 @@
+package pl.hackerspace.nfz;
+
+public class SpaReferralReader implements ISpaReferralReader {
+
+	public int getNumberInQueue(String referralId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
