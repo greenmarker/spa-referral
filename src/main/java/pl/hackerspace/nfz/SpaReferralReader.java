28ed2deb45ab355cfe27d23b3b809eabf6679153
kamilkurop <kamilkurop@gmail.com>
2015-03-22 17:04:58 +0100

    Refactoring: for better readability, extracted code necessary for establishing TLS connection into separate methods. Updated url to first page, which parser has to read.

--- src/main/java/pl/hackerspace/nfz/SpaReferralReader.java
+++ src/main/java/pl/hackerspace/nfz/SpaReferralReader.java
@@ -2,58 +2,25 @@ package pl.hackerspace.nfz;
 
 import javax.net.ssl.*;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.security.KeyStore;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
 public class SpaReferralReader implements ISpaReferralReader {
 
-    public static final String NFZ = "https://skierowania.nfz.gov.pl/";
+    public static final String NFZ = "https://skierowania.nfz.gov.pl/ap-przegl-skier/servlet/skierowania/show_app";
     public static final String CACERTS_FILE_LOCATION = "cacerts.jks";
     public static final String CACERTS_FILE_PASSWORD = "changeit";
 
 	public int getNumberInQueue(String referralId) throws Exception {
         System.setProperty("javax.net.debug", "ssl");
-        //spareferralreader
 
-        KeyStore ks = KeyStore.getInstance("JKS");
-        InputStream ksIs = SpaReferralReader.class.getResourceAsStream(CACERTS_FILE_LOCATION);
-        try {
-            ks.load(ksIs, CACERTS_FILE_PASSWORD.toCharArray());
-        } finally {
-            if (ksIs != null) {
-                ksIs.close();
-            }
-        }
-
-        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-        kmf.init(ks, CACERTS_FILE_PASSWORD.toCharArray());
-
-        // Create a trust manager that does not validate certificate chains
-        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                return null;
-            }
-            public void checkClientTrusted(X509Certificate[] certs, String authType) {
-            }
-            public void checkServerTrusted(X509Certificate[] certs, String authType) {
-            }
-        } };
-        // Install the all-trusting trust manager
-        final SSLContext sc = SSLContext.getInstance("SSL");
-        sc.init(kmf.getKeyManagers(), trustAllCerts, new java.security.SecureRandom());
-        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-        // Create all-trusting host name verifier
-        HostnameVerifier allHostsValid = new HostnameVerifier() {
-            public boolean verify(String hostname, SSLSession session) {
-                return true;
-            }
-        };
-
-        // Install the all-trusting host verifier
-        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        HttpsURLConnection.setDefaultSSLSocketFactory(getSocketFactory(CACERTS_FILE_LOCATION, CACERTS_FILE_PASSWORD));
+        HttpsURLConnection.setDefaultHostnameVerifier(getAllTrustingHostnameVerifier());
 
         URL url = new URL(NFZ);
         try (BufferedReader in = new BufferedReader(
@@ -73,8 +40,55 @@ public class SpaReferralReader implements ISpaReferralReader {
 
 
 
+
+
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
+    private SSLSocketFactory getSocketFactory(String certsFileLocation, String certsFilePassword) throws KeyStoreException, IOException, NoSuchAlgorithmException, CertificateException, UnrecoverableKeyException, KeyManagementException {
+        // Install the all-trusting trust manager
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(loadKeyManagers(certsFileLocation, certsFilePassword), createTrustAllCertsManager(), new SecureRandom());
+        return sc.getSocketFactory();
+    }
+
+    private KeyManager[] loadKeyManagers(String certsFileLocation, String certsFilePassword) throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException {
+        KeyStore ks = KeyStore.getInstance("JKS");
+        try (InputStream ksIs = SpaReferralReader.class.getResourceAsStream(certsFileLocation)) {
+            ks.load(ksIs, certsFilePassword.toCharArray());
+        }
+
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        kmf.init(ks, certsFilePassword.toCharArray());
+        return kmf.getKeyManagers();
+    }
+
+    /**
+     *
+     * @return a trust manager that does not validate certificate chains
+     */
+    private TrustManager[] createTrustAllCertsManager() {
+        return new TrustManager[] { new X509TrustManager() {
+            public X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        } };
+    }
+
+    /**
+     * @return all-trusting host name verifier
+     */
+    private HostnameVerifier getAllTrustingHostnameVerifier() {
+        return new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+    }
 
 }
